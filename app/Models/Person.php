8719101d625b8394aeabca4b70cc28616d5eaa4c
Hugosga <98834446+Hugosga@users.noreply.github.com>
2026-03-04 00:08:35 -0500

    Creating Models Employee, Person, Student

---
+++ app/Models/Person.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Person extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function employee()
+    {
+        return $this->hasOne(Employee::class);
+    }
+
+    public function student()
+    {
+        return $this->hasOne(Student::class);
+    }
+}
---
+++ database/migrations/2026_03_03_050102_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('person_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('student_code')->unique();
+            $table->date('enrollment_date');
+            $table->string('career')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
